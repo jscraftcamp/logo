b5aac69b597e3964f141fdfa290c1ce1bfc6d2ff
wolframkriesing <wolfram+github@kriesing.de>
2018-04-27 17:11:04 +0200

    Extract fn for "simple" case.

--- src/years-colors.spec.js
+++ src/years-colors.spec.js
@@ -1,12 +1,16 @@
 const {unit: it, spec: describe} = require('kavun');
 const assert = require('assert');
 
+const noop = () => {};
+
 const cloneDiamonds = (cloneDiamond, cloneDiamondWithColor) => {
   for (var row = 0; row < 11; row++) for (var column = 0; column < 6; column++) {
     cloneDiamond(row, column);
     cloneDiamondWithColor(3, 5, '#ff9800')
   }
 }
+
+const cloneDiamondsWithoutColorHandling = cloneDiamond => cloneDiamonds(cloneDiamond, noop);
 
 const buildSpy = () => {
   const spy = () => {
@@ -20,7 +24,7 @@ describe('Years config', () => {
   describe('clone diamond shapes', () => {
     it('for all 11 rows and 6 columns', () => {
       const cloneDiamondFn = buildSpy();
-      cloneDiamonds(cloneDiamondFn);
+      cloneDiamondsWithoutColorHandling(cloneDiamondFn);
       assert.equal(cloneDiamondFn.numberOfCalls, 11*6);
     });
     it('for diamond of 2016 call it with pos 3x5 and color #ff9800', () => {
