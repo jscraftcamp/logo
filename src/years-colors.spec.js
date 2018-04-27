68367175a6593a39f81a975e099da34775c228f4
wolframkriesing <wolfram+github@kriesing.de>
2018-04-27 15:08:06 +0200

    Extract fn.

--- src/years-colors.spec.js
+++ src/years-colors.spec.js
@@ -1,6 +1,12 @@
 const {unit: it, spec: describe} = require('kavun');
 const assert = require('assert');
 
+const cloneDiamonds = (cloneDiamondFn) => {
+  for (var row = 0; row < 11; row++) for (var column = 0; column < 6; column++) {
+    cloneDiamondFn(row, column);
+  }
+}
+
 describe('Years config', () => {
   describe('clone diamond shapes', () => {
     it('for all 11 rows and 6 columns', () => {
@@ -8,10 +14,7 @@ describe('Years config', () => {
         cloneDiamondFn.numberOfCalls++;
       };
       cloneDiamondFn.numberOfCalls = 0;
-      
-      for (var row=0; row<11; row++) for (var column=0; column<6; column++) {
-        cloneDiamondFn(row, column);
-      }
+      cloneDiamonds(cloneDiamondFn);
       assert.equal(cloneDiamondFn.numberOfCalls, 11*6);
     });
   });
