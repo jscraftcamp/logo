8561c8c7af5ded4ecf695b232b13b89ad7716de6
wolframkriesing <wolfram+github@kriesing.de>
2018-04-27 15:07:04 +0200

    Make sure all diamonds are cloned, all 11x6.

--- src/years-colors.spec.js
+++ src/years-colors.spec.js
@@ -2,7 +2,17 @@ const {unit: it, spec: describe} = require('kavun');
 const assert = require('assert');
 
 describe('Years config', () => {
-  it('works?', () => {
-    assert(false);
+  describe('clone diamond shapes', () => {
+    it('for all 11 rows and 6 columns', () => {
+      const cloneDiamondFn = () => {
+        cloneDiamondFn.numberOfCalls++;
+      };
+      cloneDiamondFn.numberOfCalls = 0;
+      
+      for (var row=0; row<11; row++) for (var column=0; column<6; column++) {
+        cloneDiamondFn(row, column);
+      }
+      assert.equal(cloneDiamondFn.numberOfCalls, 11*6);
+    });
   });
 });
